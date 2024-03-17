3c63f48b84ab7837f7e286c7a7638392c597b3b7
rohanb202 <rohanb22250@gmail.com>
2024-03-18 04:46:21 +0530

    added logout

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,10 +14,25 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { useRouter } from "next/router";
 import { useRecoilState } from "recoil";
 import { userState } from "../atoms/userAtom";
 export default function Navbar() {
     const [user, setUser] = useRecoilState(userState);
+    const router=useRouter();
+    function handleLogut(){
+        setUser(null);
+        router.push('/');
+    }
   return (
     <>
     <div className="flex items-center justify-between p-4 space-x-2 border-b-2 ">
@@ -51,6 +66,35 @@ export default function Navbar() {
                     Sign Up
                 </Link>
             </div>}
+           {user && 
+           <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <Avatar className="w-8 h-8 ">
+                        <AvatarImage  src="https://github.com/shadcn.png" />
+                        <AvatarFallback>K</AvatarFallback>                
+                    </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuLabel>
+                        <Link href={`/${user.name}`}>
+                            @{user.name}
+                        </Link>
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                    <Link href={`/models/new`}>
+                            + New Model
+                        </Link> 
+                    </DropdownMenuItem>                    
+                    <DropdownMenuItem>
+                    <button onClick={handleLogut}>
+                            Logout
+                    </button> 
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            
+            }
         </div>
         <Drawer >
             <DrawerTrigger asChild>
@@ -59,7 +103,28 @@ export default function Navbar() {
                  </Button>
             </DrawerTrigger>
             <DrawerContent>
-            <div className="flex flex-col items-center p-5 space-y-5 border-b-2">                
+            <div className="flex flex-col items-center p-5 space-y-5 border-b-2">
+            {user && 
+                <div className="flex flex-col items-center justify-start w-full">                    
+                    <Avatar >
+                        <AvatarImage  src="https://github.com/shadcn.png" />
+                        <AvatarFallback>K</AvatarFallback>
+                    </Avatar>
+                    <Link href={`/${user.name}`}>
+                        <Button className="font-semibold text-black bg-white hover:bg-black/10">                            
+                        @{user.name}
+                        </Button>
+                    </Link>                    
+                </div>
+            
+            }  
+            {user && <DrawerClose asChild>
+                    <Link href={`/models/new`}>
+                        <Button className="w-[90vw] ">                            
+                                + New Model
+                        </Button>
+                    </Link>                
+                </DrawerClose>}              
                 <DrawerClose asChild>
                     <Link href={'/models'}>
                         <Button className="w-[90vw] ">                            
@@ -67,6 +132,13 @@ export default function Navbar() {
                         </Button>
                     </Link>                
                 </DrawerClose>
+                {user && <DrawerClose asChild>
+                    
+                        <Button onClick={handleLogut} className="w-[90vw] ">                            
+                                Log out
+                        </Button>
+                                   
+                </DrawerClose>}  
                 {!user && <DrawerClose asChild>
                     <Link href={'/login'}>
                         <Button className="w-[90vw] ">                            
@@ -82,6 +154,7 @@ export default function Navbar() {
                         </Button>
                     </Link>                
                 </DrawerClose>}
+                
             </div>
             </DrawerContent>
         </Drawer>
