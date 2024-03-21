25553c51cbbf711d3dd73f10041afc8e425d76b1
rohanb202 <rohanb22250@gmail.com>
2024-03-22 02:31:07 +0530

    themebug fix

--- components/Hero.js
+++ components/Hero.js
@@ -5,11 +5,17 @@ import kong2 from '../public/assets/images/kong2.webp'
 
 import {StarIcon} from '@heroicons/react/24/solid'
 import { useTheme } from 'next-themes'
+import { useState } from 'react';
+import { useEffect } from 'react';
 export default function Hero() {
   let {theme}=useTheme();
-
+  const[themeX,setThemeX]=useState('light');
+  useEffect(()=>{
+    setThemeX(theme);
+  },[theme])
+  // console.log(theme);
   return (
-    <section className={`h-screen mx-5 my-6 text-white  backdrop-blur-sm ${theme=='dark'?" border-[1px] bg-gradient-to-l from-slate-100 to-slate-800 ":" bg-gradient-to-l from-slate-700 to-slate-900 "}  md:mx-10 rounded-3xl`}>
+    <section  className={`h-screen mx-5 my-6 text-white  backdrop-blur-sm  md:mx-10 rounded-3xl ${(themeX=='dark')?` border-[1px] bg-gradient-to-l from-slate-100 to-slate-800 `:` bg-gradient-to-l from-slate-700 to-slate-900 `} `}>
         <div className='flex flex-col items-center justify-between h-full md:justify-between md:flex-row'>
             <div  className='flex flex-col items-center p-10 mt-2 md:space-y-6 md:items-start'>
                 <h2 className='text-2xl md:text-3xl '>Kong app</h2>
